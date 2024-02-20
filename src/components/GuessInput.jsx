9c77e221d893c65231673bdcc94741aebe4f5b56
Louis F <72155852+itsmelouis@users.noreply.github.com>
2024-02-20 09:01:42 +0100

    fix: changed input guess word length

--- src/components/GuessInput.jsx
+++ src/components/GuessInput.jsx
@@ -17,7 +17,7 @@ const GuessInput = () => {
     <div>
       <input 
       className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-300 focus:border-green-300 block p-2.5'
-      type="text" maxLength={1} value={guess} onChange={e => setGuess(e.target.value)} />
+      type="text" value={guess} onChange={e => setGuess(e.target.value)} />
       <button className='focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2' onClick={handleGuess}>Guess</button>
     </div>
   );
